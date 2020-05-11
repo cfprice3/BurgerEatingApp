5ba4ad667a38044f025ea6fc0b3578ca70ffa8eb
cfprice3 <cfprice3@gmail.com>
2020-05-11 18:16:13 -0400

    edited connection in server.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+  connection = mysql.createConnection({
   host: "ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "p9lmh464hgxscar8",
   password: "if9nbyinh57ptvwp",
   database: "r3ia6y85xbnesgpd"
-});
+});};
 
 connection.connect(function(err) {
   if (err) {
